80eb9a6fb44fedd2be33feb0d74cb250fb4d970d
Armando7101 <164431619+Armando7101@users.noreply.github.com>
2025-02-21 18:05:14 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,119 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Inicio<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Acerca de Nosotros</a>
+                        </li>
+						<li className="nav-item">
+                            <a className="nav-link" href="#">Servicios</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+const Jumbotron = () => {
+    return (
+        <div className="mb-4 bg-secondary text-white rounded-3 mt-4">
+            <div className="container-fluid py-0 py-md-5 text-start">
+                <h1 className="display-5 fw-bold">Bienvenido!</h1>
+                <p className="col-md-16 fs-3">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore facere distinctio voluptates quia officiis unde nobis neque veniam hic. Sequi totam laudantium, velit a quaerat deleniti blanditiis facere reiciendis cumque!
+                </p>
+                <button className="btn btn-primary btn-lg" type="button">
+                    Comenzar
+                </button>
+            </div>
+        </div>
+    );
+};
+
+const Card = (props) => {
+    const { image, title, description } = props.cards;
+    return (
+        <div className="card h-100">
+            <img src={image} className="card-img-top" style={{ height: '200px', objectFit: 'cover' }} alt={title} />
+            <div className="card-body">
+                <h5 className="card-title fw-bold">{title}</h5>
+                <p className="card-text">{description}</p>
+                <hr style={{ border: 'none', height: '1px', backgroundColor: '#343a40', margin: '0 -15px', marginBottom: '20px' }} />
+                <button className="btn btn-primary">
+                    Información
+                </button>
+            </div>
+        </div>
+    );
+};
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-white text-center py-4">
+            <div className="container">
+                <p> <small>Copyright &copy; Your Website {new Date().getFullYear()}</small></p>
+            </div>
+        </footer>
+    );
+};
+
+const cardData = [
+    {
+        image: "https://photos.bandsintown.com/thumb/9823920.jpeg",
+        title: "Random",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore facere distinctio voluptates quia officiis unde nobis neque veniam hic. Sequi totam laudantium, velit a quaerat deleniti blanditiis"
+    },
+    {
+        image: "https://quantumbase.com/wp-content/uploads/2018/06/138913782_whitebg-300x300.jpg",
+        title: "Gameboy",
+        description: "La Game Boy es una consola de mano portátil fabricada por Nintendo, alimentadas con pilas (a excepción del modelo SP y Micro), lanzada inicialmente en 1989. Es uno de los sistemas más vendidos hasta la fecha."
+    },
+    {
+        image: "https://console.kr-asia.com/wp-content/uploads/2019/07/Coding-300x300.jpg",
+        title: "Bootcamp",
+        description: "Los bootcamps son programas de estudios intensivos e inmersivos que tienen como objetivo desarrollar habilidades tecnológicas para profesionales que se desempeñan en el área de la ingeniería informática."
+    },
+	{
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfKkGTj4jWXn7qyqw9TGVs0Be_YkqyN6oTmQ&s",
+        title: "REACT",
+        description: "React es una biblioteca Javascript de código abierto diseñada para crear interfaces de usuario con el objetivo de facilitar el desarrollo de aplicaciones en una sola página."
+    }
+];
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+			<div className="container">
+				<Jumbotron />
+                <div className="row justify-content-center">
+                    {cardData.map((card, index) => (
+                        <div className="col-md-3" key={index}>
+                            <Card cards={card} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+			<Footer />
+        </div>
+    );
 };
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,69 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PropTypes from 'prop-types';
 
+//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+// index.css'
 import '../styles/index.css'
 
-function ContadorDificil(props){
-  return (
-  <div className = "container">
-    <div className = "reloj">
-      <i className="fa-regular fa-clock"></i>
-    </div>
-    <div className = "sexto">{props.numeroSeis % 10}</div>
-    <div className = "quinto">{props.numeroCinco % 10}</div>
-    <div className = "cuarto">{props.numeroCuatro% 10}</div>
-    <div className = "tercero">{props.numeroTres % 10}</div>
-    <div className = "segundo">{props.numeroDos % 10}</div>
-    <div className = "primero">{props.numeroUno % 10}</div>
-  </div>);
+// components
+import Home from './components/Home';
 
-}
-
-ContadorDificil.propTypes = {
-  numeroSeis: PropTypes.number,
-  numeroCinco: PropTypes.number,
-  numeroCuatro: PropTypes.number,
-  numeroTres: PropTypes.number,
-  numeroDos: PropTypes.number,
-  numeroUno: PropTypes.number
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-function Logica() {
-  const [contador, setContador] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setContador(prevContador => prevContador + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const seis = Math.floor(contador / 100000);
-  const cinco = Math.floor(contador / 10000);
-  const cuatro = Math.floor(contador / 1000);
-  const tres = Math.floor(contador / 100);
-  const dos = Math.floor(contador / 10);
-  const uno = Math.floor(contador / 1);
-
-  return (
-    <React.StrictMode>
-      <ContadorDificil 
-        numeroSeis={seis} 
-        numeroCinco={cinco} 
-        numeroCuatro={cuatro} 
-        numeroTres={tres} 
-        numeroDos={dos} 
-        numeroUno={uno} 
-      />
-    </React.StrictMode>
-  );
-}
-
-root.render(<Logica />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+)
